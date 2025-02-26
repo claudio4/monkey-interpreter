f8466ca6a1f6afb6ea7a21135690c5314e45b398
Claudio Yanes <me@claudio4.com>
2025-02-26 02:28:08 +0000

    Add conditionals support

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -13,10 +13,14 @@ var (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	case *ast.InfixExpression:
 		left := Eval(node.Left)
 		right := Eval(node.Right)
@@ -119,5 +123,30 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
