0bb61a4827c539fc980dd78dfa0fe3e6927790f4
Claudio Yanes <me@claudio4.com>
2025-03-08 23:21:56 +0000

    Add quote and unquote

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -26,6 +26,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
 	case *ast.CallExpression:
+		if node.Function.TokenLiteral() == "quote" {
+			return quote(node.Arguments[0], env)
+		}
 		function := Eval(node.Function, env)
 		if isError(function) {
 			return function
---
+++ evaluator/quote.go
@@ -0,0 +1,69 @@
+package evaluator
+
+import (
+	"fmt"
+
+	"github.com/claudio4/going-monkey/ast"
+	"github.com/claudio4/going-monkey/object"
+	"github.com/claudio4/going-monkey/token"
+)
+
+func quote(node ast.Node, env *object.Environment) object.Object {
+	node = unquoteModifier(node, env)
+	return &object.Quote{Node: node}
+}
+
+func unquoteModifier(baseNode ast.Node, env *object.Environment) ast.Node {
+	return ast.Modify(baseNode, func(node ast.Node) ast.Node {
+		call := toUnquteCallExpression(node)
+		if call == nil {
+			return node
+		}
+
+		if len(call.Arguments) != 1 {
+			return node
+		}
+
+		fmt.Println("hit unqute")
+		evaluated := Eval(call.Arguments[0], env)
+		return convertObjectToASTNode(evaluated)
+	})
+}
+
+// toUnquteCallExpression receives a node and returns the CallExpression if it's a call to unquote.
+// if node is any other node type it returns nil
+func toUnquteCallExpression(node ast.Node) *ast.CallExpression {
+	callExpression, ok := node.(*ast.CallExpression)
+	if !ok {
+		return nil
+	}
+
+	if callExpression.Function.TokenLiteral() == "unquote" {
+		return callExpression
+	} else {
+		return nil
+	}
+}
+
+func convertObjectToASTNode(obj object.Object) ast.Node {
+	switch obj := obj.(type) {
+	case *object.Boolean:
+		var t token.Token
+		if obj.Value {
+			t = token.Token{Type: token.TRUE, Literal: "true"}
+		} else {
+			t = token.Token{Type: token.FALSE, Literal: "false"}
+		}
+		return &ast.Boolean{Token: t, Value: obj.Value}
+	case *object.Integer:
+		t := token.Token{
+			Type:    token.INT,
+			Literal: fmt.Sprintf("%d", obj.Value),
+		}
+		return &ast.IntegerLiteral{Token: t, Value: obj.Value}
+	case *object.Quote:
+		return obj.Node
+	default:
+		return nil
+	}
+}
